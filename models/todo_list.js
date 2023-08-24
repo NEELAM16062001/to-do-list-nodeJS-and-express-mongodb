63f45470378ee67309b7b2a1a21c4073f30586f6
NEELAM16062001 <110402550+NEELAM16062001@users.noreply.github.com>
2023-08-24 10:18:05 +0530

    Update todo_list.js

--- models/todo_list.js
+++ models/todo_list.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose');
-const todoSchema = new mongoose.Schema({
-    desc:{
-        type:String,
-        required:true
-    },
-    category:{
-        type:String,
-        required:true
-    },
-    dueDate:{
-        type:String,
-        required:true
-    }   
-})
+const mongoose = require('mongoose'); // This line imports the Mongoose library.
 
-const TodoLists = mongoose.model('TodoLists', todoSchema);
-module.exports  = TodoLists;
+const todoSchema = new mongoose.Schema({ // This line creates a new Mongoose schema.
+desc: { // This field defines the description of the todo list.
+type: String, // The type of the field is String.
+required: true // This field is required.
+},
+category: { // This field defines the category of the todo list.
+type: String, // The type of the field is String.
+required: true // This field is required.
+},
+dueDate: { // This field defines the due date of the todo list.
+type: String, // The type of the field is String.
+required: true // This field is required.
+}
+});
+
+const TodoLists = mongoose.model('TodoLists', todoSchema); // This line creates a Mongoose model named TodoLists.
+
+module.exports = TodoLists; // This line exports the TodoLists model.
