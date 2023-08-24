82f2512b659c06b779483f4c024885e36d3802c9
NEELAM16062001 <110402550+NEELAM16062001@users.noreply.github.com>
2023-08-24 10:20:31 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todo_list_db');
-const db = mongoose.connection;
-// error
-db.on('error',console.error.bind(console,'erroe connecting to db'));
-// up and running then message
-db.once('open',function(){
-    console.log('Success fully connected to the database')
+const mongoose = require('mongoose'); // This line imports the Mongoose library.
+
+mongoose.connect('mongodb://localhost/todo_list_db'); // This line connects to the MongoDB database named "todo_list_db".
+
+const db = mongoose.connection; // This line gets the Mongoose connection object.
+
+// This code listens for the error event and logs the error to the console.
+db.on('error', console.error.bind(console, 'Error connecting to db'));
+
+// This code listens for the open event and logs a message to the console when the connection is successful.
+db.once('open', function() {
+console.log('Successfully connected to the database');
 })
